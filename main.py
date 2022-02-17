80c99a606dee576c2ab544db7bb0de818746d9e8
308 <308@academicos.cesde.edu.co>
2022-02-17 07:27:09 -0500

    se crea hola mundo con python

---
+++ main.py
@@ -0,0 +1,33 @@
+#Esto es un comentario de linea
+
+'''
+Cometario de bloque
+'''
+
+#Salida por consola
+#print("Hola mundo Python")
+
+#Variables o entradas
+nameUser = 'Marquitos'
+ageUser = 36
+heightUser = 1.72
+IsaGreenSupporter = True
+
+#Salida de variables
+'''
+print(nameUser)
+print('Buenos dias',nameUser)
+print(f'Buenos dias {nameUser}')
+
+print(f'Buenos dias señor: {nameUser} su edad es: {ageUser} su altura es: {heightUser} le gusta el verde?: {IsaGreenSupporter}' )
+print(f'usted imprimio el numero {total}')
+'''
+
+
+#Recibiendo variables de consola
+numero1 = int (input('Digita un numero entero: '))
+numero2 = int (input('Digita un otro numero: '))
+total = numero1 + numero2
+print(f'el total es: {total}')
+print(f'{numero1} + {numero2} = {total}')
+
