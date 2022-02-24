5a2026f9f495c984c72ac5e0c5e7af0e3a397cb2
308 <308@academicos.cesde.edu.co>
2022-02-24 07:41:19 -0500

    se agregan rutinas if y while introducidas a python

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ Cometario de bloque
 #print("Hola mundo Python")
 
 #Variables o entradas
-nameUser = 'Marquitos'
-ageUser = 36
-heightUser = 1.72
-IsaGreenSupporter = True
+#nameUser = 'Marquitos'
+#ageUser = 36
+#heightUser = 1.72
+#IsaGreenSupporter = True
 
 #Salida de variables
 '''
@@ -25,9 +25,57 @@ print(f'usted imprimio el numero {total}')
 
 
 #Recibiendo variables de consola
-numero1 = int (input('Digita un numero entero: '))
-numero2 = int (input('Digita un otro numero: '))
-total = numero1 + numero2
-print(f'el total es: {total}')
-print(f'{numero1} + {numero2} = {total}')
+#numero1 = int (input('Digita un numero entero: '))
+#numero2 = int (input('Digita un otro numero: '))
+#total = numero1 + numero2
+#print(f'el total es: {total}')
+#print(f'{numero1} + {numero2} = {total}')
+
+
+#Tomando decisiones
+#numero = int (input("Por favor digita un numero : "))
+#print(f'El numero digitado fue: {numero}')
+
+#if(numero>=0):
+#    print(f"El numero que digitaste es positivo")
+#else:
+#    print(f"El numero que digitaste es negativo")
+#    print("oe... fuck you men")
+
+#Hidroituango
+'''
+nivelAgua = int(input('Digite el nivel de agua ;) '))
+
+if(nivelAgua<200):
+    print(f"Hay muy poca agua en la represa")
+elif(nivelAgua>=200 and nivelAgua<450):
+    print(f"Hay buena cantidad de agua mi so")
+else:
+    print(f"Hay mucha agua ojo")
+'''
+
+#Calcular estacion segun
+'''
+mes = input('Digite el mes: ')
+
+if(mes== "enero" or mes== "febrero" or mes== "marzo"):
+    print(f"estamos en invierno")
+elif(mes== "abril" or mes== "mayo" or mes== "junio"):
+    print(f"estamos en verano")
+elif(mes== "julio" or mes== "agosto" or mes== "septiembre"):
+    print(f"estamos en otoño")
+elif(mes== "octubre" or mes== "noviembre" or mes== "diciembre"):
+    print(f"estamos en primavera")
+else:
+    print(f"uste que digito??")
+'''
+
+#While/for bucles, ciclos, loops, repeticiones
+contador=0
+
+while(contador<10):
+    contador+=1
+    print(contador)
+else:
+    print("termino el ciclo")
 
